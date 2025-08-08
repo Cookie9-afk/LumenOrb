e03b054cea7c0244da3c96194285f7d8a5513ff6
Cookie9-afk <alessandroSTP@outlook.it>
2025-08-08 14:30:23 +0200

    added lambdynamiclights compatibility and fixed mod options

--- src/main/java/lumenorbmod/event/EventRegistry.java
+++ src/main/java/lumenorbmod/event/EventRegistry.java
@@ -3,6 +3,7 @@ package lumenorbmod.event;
 import lumenorbmod.item.LumenOrbItem;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 
 import static lumenorbmod.utils.LumenOrbBehavior.pickupTorch;
 
@@ -13,9 +14,13 @@ public final class EventRegistry {
     // listens for the player starting to break a block
     public static void register() {
         AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
-            // check if holding the orb
-            if (!world.isClient && player.getStackInHand(hand).getItem() instanceof LumenOrbItem) {
-                return pickupTorch(player, world, hand, pos);
+            // check if holding the orb from the server
+            if (!world.isClient) {
+                if (player.getOffHandStack().getItem() instanceof LumenOrbItem) {
+                    return pickupTorch(player, world, Hand.OFF_HAND, pos);
+                } else if (player.getStackInHand(hand).getItem() instanceof LumenOrbItem) {
+                    return pickupTorch(player, world, hand, pos);
+                }
             }
             return ActionResult.PASS;
         });
